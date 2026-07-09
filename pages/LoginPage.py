dea7429a786d654a8eecc4daab1fe5f124191b32
José Luis Rangel Zayas <joseluis.rangelz06@gmail.com>
2026-07-09 01:04:24 -0600

    Agregando issues para evidencia 2

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -18,3 +18,7 @@ class LoginPage:
         pass_field = self.wait.until(EC.visibility_of_element_located((By.NAME, "password")))
         pass_field.send_keys(password)
         pass_field.send_keys(Keys.RETURN) # <-- Forzamos el envío del formulario
+
+    print("Iniciando login...") # Issue 1: Evitar el uso de print()
+    variable_falsa = 100        # Issue 2: Variable local sin usar
+    # time.sleep(5)             # Issue 3: Bloque de código comentado
